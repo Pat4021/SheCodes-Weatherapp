b0e3d4f089328bd241ec6568eef9b2ec00b3a7ad
Pat4021 <86599763+Pat4021@users.noreply.github.com>
2021-08-16 15:28:53 +0100

    search Engine

--- src/app.js
+++ src/app.js
@@ -32,10 +32,24 @@
      iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
      iconElement.setAttribute("alt", response.data.weather[0].description)
  }
- 
+
+function search(city) {
+
  let apiKey = "061254958b012ab279040ab26822d7e4";
- let city ="New York";
  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-
  axios.get(apiUrl).then(displayTemperature); 
+
+}
+
+ function handleSubmit(event) {
+     event.preventDefault();
+     let cityInputElement = document.querySelector("#city-input");
+     search(cityInputElement.value)
+
+ }
+
+ search("New York");
+ 
+
+ let form = document.querySelector("search-form"); 
+ form.addEventListener("submit", handleSubmit);
