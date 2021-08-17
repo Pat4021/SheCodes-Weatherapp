45d2b2e69e7f4f6c89870009bf0e8617aaca0ce5
Pat4021 <86599763+Pat4021@users.noreply.github.com>
2021-08-17 17:10:24 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@
  }
 
  function displayForecast(response) {
-      console.log(response.data.daily);
+      //console.log(response.data.daily);
      
       let forecastElement = document.querySelector("#forecast");
 
@@ -40,7 +40,6 @@ forecastElement.innerHTML = forecastHTML;
  }
 
 function getForecast(coordinates){
-  console.log(coordinates);
   let apiKey = "061254958b012ab279040ab26822d7e4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast); 
